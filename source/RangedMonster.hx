de8acdf521f23c2ac884438b5800fc166fe00d31
Jean-Jacques <jeanjacques.strydom34@gmail.com>
2021-03-08 08:26:20 +0200

    Removed monster interface

--- source/RangedMonster.hx
+++ source/RangedMonster.hx
@@ -3,23 +3,19 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
-class RangedMonster extends FlxSprite implements IMonster
+class RangedMonster extends FlxSprite
 {
-	private var _damage_per_seconds:Float;
-	private var _armor:Float;
-	private var _movement_speed:Float;
-	private var _target:FlxObject;
-	private var _projectileType:ProjectileType;
-	private var _lastShot:Float;
-	private var _shotVar:Float;
+	private var _armor:Float; // The amount of armor the monster has.
+	private var _target:FlxObject; // The monster's target.
+	private var _projectileType:ProjectileType; // The type of projectile the monster uses.
+	private var _lastShot:Float; // The last time the monster shot at the target.
+	private var _shotVar:Float; // The variable for when the monster should fire.
 
 	public function new(x:Float, y:Float, target:FlxObject, projectileType:ProjectileType)
 	{
 		super(x, y);
 
-		_damage_per_seconds = 0;
 		_armor = 0;
-		_movement_speed = 0;
 		this._target = target;
 		this._projectileType = projectileType;
 		_lastShot = 0;
@@ -28,16 +24,29 @@ class RangedMonster extends FlxSprite implements IMonster
 		makeGraphic(20, 20, FlxColor.BLUE);
 	}
 
+	/**
+	 * Getter for the monster's target.
+	 * @return FlxObject
+	 */
 	public function getTarget():FlxObject
 	{
 		return this._target;
 	}
 
+	/**
+	 * Getter for the type of projectile the monster uses.
+	 * @return ProjectileType
+	 */
 	public function getProjectileType():ProjectileType
 	{
 		return this._projectileType;
 	}
 
+	/**
+	 * Function to check whether or not the monster should fire a shot.
+	 * @param elapsed The time elapsed between frames (delta time).
+	 * @return Bool Whether or not the monster should fire.
+	 */
 	public function shouldFire(elapsed:Float):Bool
 	{
 		_lastShot += elapsed;
