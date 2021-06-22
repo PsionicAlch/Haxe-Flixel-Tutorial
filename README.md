7a8e41677a899f32f4b7aa0824076e08d8c509b2
Jean-Jacques <jeanjacques.strydom34@gmail.com>
2021-06-22 11:27:57 +0200

    Worked on melee test state

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 An introductory tutorial on how to use Haxe Flixel
 
-In this tutorial we make it easier for ourselves to test new features by setting up multiple testing states where we can try out new features.
+In this tutorial we work on setting up the testing state for melee characters
 
-![Preview Picture](https://github.com/Wolfman13/Haxe-Flixel-Tutorial/blob/Tutorial-18/Tutorial_18.png?raw=true)
+![Preview Picture](https://github.com/Wolfman13/Haxe-Flixel-Tutorial/blob/Tutorial-19/Tutorial_19.png?raw=true)
--- source/MeleeTestState.hx
+++ source/MeleeTestState.hx
@@ -1,8 +1,78 @@
+import MeleeMonster.MeleeType;
+import Projectile.ProjectileType;
+import flixel.FlxG;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
 
 class MeleeTestState extends FlxState
 {
+    private var _player:Player;
+    private var _meleeMonsters:FlxTypedGroup<MeleeMonster>;
+    private var _projectiles:FlxTypedGroup<Projectile>;
+
     override function create() {
+        FlxG.worldBounds.set(-5000, -5000, 10000, 10000);
+
+        _projectiles = new FlxTypedGroup<Projectile>();
+        add(_projectiles);
+
+        _player = new Player(0, 0);
+        add(_player);
+
+        FlxG.camera.follow(_player, TOPDOWN, 1);
+
+        _meleeMonsters = new FlxTypedGroup<MeleeMonster>();
+        add(_meleeMonsters);
+
+        for (_ in 0...1000)
+        {
+            _meleeMonsters.add(new MeleeMonster(Random.float(-5000, 5000), Random.float(-5000, 5000), _player, MeleeType.ZOMBIE));
+            _meleeMonsters.add(new MeleeMonster(Random.float(-5000, 5000), Random.float(-5000, 5000), _player, MeleeType.SKELETON));
+        }
+
         super.create();
+    }
+
+    override function update(elapsed:Float) {
+        shoot();
+
+        _projectiles.forEachAlive(handleProjectiles);
+
+        FlxG.overlap(_meleeMonsters, _projectiles, handleMonsterProjectileCollisions);
+        FlxG.collide(_player, _meleeMonsters, handlePlayerMonsterCollisions);
+
+        super.update(elapsed);
+    }
+
+    private function shoot()
+    {
+        if (FlxG.mouse.justPressed)
+        {
+            var mousePos = FlxG.mouse.getPosition();
+            _projectiles.add(new Projectile(_player.getGraphicMidpoint().x, _player.getGraphicMidpoint().y, mousePos, ProjectileType.FIRE_BOLT, _player));
+            _player.fire();
+        }
+    }
+
+    private function handleProjectiles(projectile:Projectile)
+    {
+        if (projectile.getDurationAlive() >= 2)
+        {
+            projectile.kill();
+        }
+    }
+
+    private function handleMonsterProjectileCollisions(monster:MeleeMonster, projectile:Projectile)
+    {
+        if (projectile.getSpawner() == _player)
+        {
+            monster.kill();
+            projectile.kill();
+        }
+    }
+
+    private function handlePlayerMonsterCollisions(player:Player, monster:MeleeMonster)
+    {
+        player.setPosition(0, 0);
     }
 }
